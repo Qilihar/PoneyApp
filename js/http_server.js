b1de2576e6006d3cee6635b056de99bac1f04348
Poney App <poneyapp@vps645014.ovh.net>
2019-02-03 12:38:49 +0100

    Test squelette

---
+++ js/http_server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+http.createServer(function (req, res) {
+	  res.writeHead(200, {'Content-Type': 'text/plain'});
+	  res.write('Bienvenue sur la Poney App\'\n');
+	  res.end('Alukakah mdr');
+}).listen(8080, "localhost");
+console.log('Server running at http://127.0.0.1:8080/');
+console.log('Le console log de Suki');
