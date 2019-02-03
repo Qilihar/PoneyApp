458a4b7f605d4c7d561ee72f869c5ced63953436
Qilihar <sbrulin.pro@gmail.com>
2019-02-03 13:41:57 +0100

    Commit

--- js/http_server.js
+++ js/http_server.js
@@ -2,7 +2,7 @@ var http = require('http');
 http.createServer(function (req, res) {
 	  res.writeHead(200, {'Content-Type': 'text/plain'});
 	  res.write('Bienvenue sur la Poney App\'\n');
-	  res.end('Test 123');
+	  res.end('Ceci est le Backend en Nodejs');
 }).listen(8080, "localhost");
 console.log('Server running at http://127.0.0.1:8080/');
 console.log('Le console log de Suki');
